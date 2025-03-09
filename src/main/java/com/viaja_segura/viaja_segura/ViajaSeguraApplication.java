6e034a9d0b8020f143e4a3de56a2bfa1a7f423fb
julio-ocampo <julio.ocampo@egis-mexico.com>
2025-03-09 00:45:47 -0600

    first commit

---
+++ src/main/java/com/viaja_segura/viaja_segura/ViajaSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.viaja_segura.viaja_segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajaSeguraApplication.class, args);
+	}
+
+}
